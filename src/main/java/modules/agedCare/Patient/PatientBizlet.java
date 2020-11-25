328549089fbb7c6e63e805115be5f4c9082261f1
Seema Kumari <seema.kanish10@gmail.com>
2020-11-25 10:43:38 +1030

    Formatted all files

--- src/main/java/modules/agedCare/Patient/PatientBizlet.java
+++ src/main/java/modules/agedCare/Patient/PatientBizlet.java
@@ -12,28 +12,24 @@ public class PatientBizlet extends Bizlet<Patient> {
 	 */
 	private static final long serialVersionUID = 1455780144763235289L;
 
-	
 	public Patient newInstance(Patient bean) throws Exception {
 		// TODO Auto-generated method stub
-		
-		
-		 return super.newInstance(bean) ;
 
-		
+		return super.newInstance(bean);
+
 	}
 
 	@Override
 	public void preSave(Patient bean) throws Exception {
-		
-		if(bean.getPatientID()==null)
-		{ 
-			String patientId=ModulesUtil.getNextDocumentNumber("P", Patient.MODULE_NAME, Patient.DOCUMENT_NAME, Patient.patientIDPropertyName, 4);
+
+		if (bean.getPatientID() == null) {
+			String patientId = ModulesUtil.getNextDocumentNumber("P", Patient.MODULE_NAME, Patient.DOCUMENT_NAME,
+					Patient.patientIDPropertyName, 4);
 			bean.setPatientID(patientId);
 			return;
-			
+
 		}
 		return;
 	}
 
-	
 }
